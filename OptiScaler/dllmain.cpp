a0b2bb354fa9d4d1af9dc4519c88129147b425b5
ShyVortex <angelotr@ik.me>
2026-09-20 18:36:05 +0200

    DLSSG: Add KCD2 native HDR10 compatibility quirk (PR #70/commit 34dfe6d9 port)

--- OptiScaler/dllmain.cpp
+++ OptiScaler/dllmain.cpp
@@ -1,4 +1,4 @@
-#include "pch.h"
+﻿#include "pch.h"
 #include "dllmain.h"
 
 #include "Util.h"
@@ -1317,6 +1317,9 @@ static void printQuirks(flag_set<GameQuirk>& quirks)
 
     if (quirks & GameQuirk::CreateSLOnThe2ndDevice)
         stringQuirks.push_back("Create SL on the 2nd device");
+
+    if (quirks & GameQuirk::Kcd2DlssgHdr10)
+        stringQuirks.push_back("KCD2 native HDR10 for DLSSG");
 
     state->detectedQuirks.append_range(stringQuirks);
     for (auto& stringQuirk : stringQuirks)
