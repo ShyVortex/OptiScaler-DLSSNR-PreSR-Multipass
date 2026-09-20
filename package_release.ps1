1e0697a13b8c81fc4001c9fba3215e39c9159f83
ShyVortex <angelotr@ik.me>
2026-09-20 18:43:49 +0200

    Bump version to 0.9.8

--- package_release.ps1
+++ package_release.ps1
@@ -10,7 +10,7 @@
 
 param(
     [ValidatePattern('^[A-Za-z0-9][A-Za-z0-9._-]*$')]
-    [string]$Version = "v0.9.7",
+    [string]$Version = "v0.9.8",
     [switch]$SkipBuild,
     [switch]$IncludeDlssFrameGeneration,
     [switch]$AcceptNvidiaLicenses,
