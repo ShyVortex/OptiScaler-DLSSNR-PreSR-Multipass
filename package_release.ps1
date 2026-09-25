92755553e294a32e43699739a254d6e278da8dc9
ShyVortex <angelotr@ik.me>
2026-09-25 11:26:25 +0200

    Bump version to 0.9.30

--- package_release.ps1
+++ package_release.ps1
@@ -10,7 +10,7 @@
 
 param(
     [ValidatePattern('^[A-Za-z0-9][A-Za-z0-9._-]*$')]
-    [string]$Version = "v0.9.26",
+    [string]$Version = "v0.9.30",
     [switch]$SkipBuild,
     [switch]$IncludeDlssFrameGeneration,
     [switch]$AcceptNvidiaLicenses,
